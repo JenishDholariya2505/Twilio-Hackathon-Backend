e6f4093e0b93d972743d1b0719cd88d51683a6ee
JenishDholariya <jenishdholariya2505@gmail.com>
2025-08-23 14:17:40 +0530

    init

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+// TEST
 // Debug environment variables
 console.log("=== Environment Check ===");
 console.log(
